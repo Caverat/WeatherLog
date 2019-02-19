b389680310817eec4896b72a764be15cd5af1a2b
Erik Kristoffersen <Erik.Kristoffersen@infocare.com>
2019-02-19 10:10:07 +0100

    Debug config and http status on weatherLog create

--- WeatherLogService/api/controllers/weatherLogController.js
+++ WeatherLogService/api/controllers/weatherLogController.js
@@ -3,26 +3,29 @@
 var mongoose = require('mongoose'),
     WeatherLog = mongoose.model('WeatherLog');
 
-exports.list_all_weather_logs = function(req, res){
-    WeatherLog.find({}, function(err, weatherLog) {
+exports.list_all_weather_logs = function (req, res) {
+    WeatherLog.find({}, function (err, weatherLog) {
         if (err)
             res.send(err);
         res.json(weatherLog);
     });
 };
 
-exports.create_weather_log = function(req, res) {
+exports.create_weather_log = function (req, res) {
     var new_weather_log = new WeatherLog(req.body);
-    new_weather_log.save(function(err, weatherLog) {
-        if(err)
-            res.send(err);
-        res.json(weatherLog);
+    new_weather_log.save(function (err, weatherLog) {
+        if (err)
+            res.status(422).send(err);
+        else {
+            res.set({ 'Location': '/weatherLogs/' + weatherLog._id });
+            res.status(201).json(weatherLog);
+        }
     });
 };
 
-exports.get_weather_log = function(req, res) {
-    WeatherLog.findById(req.params.weatherLogId, function(err, weatherLog) {
-        if(err)
+exports.get_weather_log = function (req, res) {
+    WeatherLog.findById(req.params.weatherLogId, function (err, weatherLog) {
+        if (err)
             res.send(err);
         res.json(weatherLog);
     });
